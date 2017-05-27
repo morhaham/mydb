5d2c1148231ebd862de8df047fc018b663685c32
Mor Haham <moring3071@gmail.com>
2017-05-28 00:47:53 +0200

    initial commit

---
+++ src/MyDB.php
@@ -0,0 +1,28 @@
+<?php
+
+/****************
+An interface for a quick database connection.
+****************/
+namespace Mor\MyDB;
+
+use \PDO;
+
+class MyDB {
+    private static $connection;
+
+    public static function connect($server_name, $username, $password, $database_name) {
+        try {
+            if(self::$connection == null) {
+                self::$connection = new PDO("mysql:host=$server_name; dbname=$database_name; charset=utf8", $username, $password);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return "Connected successfully";
+            }
+            else {
+			    return "Already connected to a database.";
+			}
+        }
+        catch(PDOException $e) {
+            return "Connection failed: ".$e -> getMessage();
+        }
+    }   
+}
